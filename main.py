b5fede5ed5e088a76ac78f5b04158969f5b72828
Gijs Koot <gijs@xomnia.com>
2016-12-19 16:35:01 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import feedparser
+import util
+
+url = 'http://feeds.nos.nl/nosnieuwseconomie'
+feed = feedparser.parse(url)
+
+for entry in feed["entries"]:
+
+    util.create_wordcloud(entry.summary, entry.title)
---
+++ util.py
@@ -0,0 +1,15 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def create_wordcloud(txt, title):
+
+    wc = WordCloud().generate(txt)
+
+    plt.figure(figsize=(18, 14))
+    plt.imshow(wc)
+
+    plt.savefig("{}.png".format(title))
+
+if __name__ == "__main__":
+
+    create_wordcloud("Test text met woorden")
